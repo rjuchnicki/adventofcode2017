e62c57a34b22e2b8cca9a3458f59812090339d81
rjuchnicki <robert.juchnicki@gmail.com>
2017-12-01 00:36:26 -0500

    clean up newline in day 1 input

--- day01/day01.py
+++ day01/day01.py
@@ -1,24 +1,20 @@
 f = open('input.txt', 'r')
-s = f.read()
+s = f.read().strip('\n')
 f.close()
+
+print(s)
 
 # Part 1
 ret = 0
-c = s[0]
-for i in range(1, len(s)):
-    if s[i] == c:
-        ret += int(c)
-    else:
-        c = s[i]
-
-if s[-2] == s[0]:
-    ret += int(s[-2])
+for i in range(0, len(s)):
+    if s[i] == s[i-1]:
+        ret += int(s[i])
 print(ret)
 
 # Part 2
 ret = 0
 n = len(s)
 for i in range(0, len(s)):
-    if s[i] == s[int(i+n/2) % (n-1)]:
+    if s[i] == s[int(i+n/2) % n]:
         ret += int(s[i])
 print(ret)
