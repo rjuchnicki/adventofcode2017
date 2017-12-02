8362d8a54cb1be01eddd0aa7921c9257eae703a8
rjuchnicki <robert.juchnicki@gmail.com>
2017-12-02 00:21:10 -0500

    cleanup Day 2

--- day02/day02.py
+++ day02/day02.py
@@ -2,21 +2,20 @@ f = open('input.txt', 'r')
 lines = [x.strip('\n') for x in f.readlines()]
 f.close()
 
-# Part 1
-ret = 0
-for l in lines:
-    nums = [int(i) for i in l.split('\t')]
-    ret += (max(nums) - min(nums))
-print(ret)
 
-# Part 2
-ret = 0
+ret1 = 0  # Part 1
+ret2 = 0  # Part 2
 for l in lines:
     nums = [int(i) for i in l.split('\t')]
-    for i in range(0, len(nums)):
-        for j in range(i+1, len(nums)):
+    ret1 += (max(nums) - min(nums))
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i == j:
+                continue
             if nums[i] % nums[j] == 0:
-                ret += (nums[i] / nums[j])
-            elif nums[j] % nums[i] == 0:
-                ret += (nums[j] / nums[i])
-print(ret)
+                ret2 += (nums[i] / nums[j])
+                break
+
+print(ret1)
+print(ret2)
